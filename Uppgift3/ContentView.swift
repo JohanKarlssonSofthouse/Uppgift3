97162cedf96fc3ce258b7eee758a4893e2134d17
Johan Karlsson <johan.karlsson@softhouse.se>
2023-10-09 07:58:47 +0200

    Initial Commit

---
+++ Uppgift3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uppgift3
+//
+//  Created by Johan Karlsson on 2023-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uppgift3/Uppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift3App.swift
+//  Uppgift3
+//
+//  Created by Johan Karlsson on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
